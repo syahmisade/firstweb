effdb5d950793350328975aae25d026394202b03
Syahmi <amuhamadsade@sheffield.ac.uk>
2024-08-21 05:37:20 +0800

    Building my own page

---
+++ syahmisade/src/pages/blog.js
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+      <div>
+        <h1>Blog</h1>
+        <p>Welcome to my blog where I share my thoughts on life, job hunting, and coding.</p>
+      </div>
+    );
+  }
+  
---
+++ syahmisade/src/pages/portfolio.js
@@ -0,0 +1,9 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+        <p>Welcome to my blog where I share my thoughts on life, job hunting, and coding.</p>
+      </div>
+    );
+  }
+  
